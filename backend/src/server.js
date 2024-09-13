2d1f3cc4175d7c74b67fd1ca29784f6d2f7d91ce
Benjamin <benjaminbelloeil03@gmail.com>
2024-09-13 01:00:40 -0600

    Refactor server.js: Remove unused code and export app

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,15 +7,11 @@ const searchRoutes = require('./routes/search');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', searchRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = app;
