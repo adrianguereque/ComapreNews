261d6ff8de1aa1f39fe624d30a0e06d48a70cc7e
Adrian Guereque <aguereque96@gmail.com>
2024-09-13 16:09:27 -0600

    agregar build para deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,6 +14,14 @@ app.use(express.json());
 
 app.use('/api', searchRoutes);
 
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all handler for any requests that don't match the API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
