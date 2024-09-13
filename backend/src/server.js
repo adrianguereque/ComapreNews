0585b1e4dd77473b9a2b20a9096f4336354094cb
Benjamin <benjaminbelloeil03@gmail.com>
2024-09-13 01:02:10 -0600

    Refactor server.js: Update app port and add console log

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,15 @@ const searchRoutes = require('./routes/search');
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', searchRoutes);
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
